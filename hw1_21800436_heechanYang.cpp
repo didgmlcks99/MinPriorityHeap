223813752c3ff5f14d7f51b01463132c0cebc7b5
didgmlcks99 <heechy06@naver.com>
2021-03-19 22:06:38 +0900

    before submimssion

--- hw1_21800436_heechanYang.cpp
+++ hw1_21800436_heechanYang.cpp
@@ -13,8 +13,8 @@ void printMenu();
 char chooseMenu();
 Element scanElement();
 void insert(Element *a, int &i, Element newElement);
-void decreaseScore(Element *a);
-void decrease(Element *a, int i, float newScore);
+void decreaseScore(Element *a, int count);
+int decrease(Element *a, int i, float newScore);
 void swap(Element *a, Element *b);
 void extractMin(Element *a, int &i);
 void minHeapify(Element *a, int size, int i);
@@ -26,22 +26,37 @@ int main(){
 
     do{
         printMenu();
-        switch(chooseMenu()){
+        switch(toupper(chooseMenu())){
             case 'I':
                 insert(heap, count, scanElement());
                 break;
             case 'D':
+                if(count <= 0){
+                    cout << "=> No data to delete" << endl << endl << endl;
+                    continue;
+                }
                 extractMin(heap, count);
                 break;
             case 'C':
-                decreaseScore(heap);
+                if(count <= 0){
+                    cout << "=> No data to decrease" << endl << endl << endl;
+                    continue;
+                }
+                decreaseScore(heap, count);
                 break;
             case 'P':
+                if(count <= 0){
+                    cout << "=> No data to print" << endl << endl << endl;
+                    continue;
+                }
                 print(heap, count);
                 break;
             case 'Q':
-                cout << "Thank you, Bye!";
+                cout << "=> Thank you, Bye!";
                 return 0;
+            default:
+                cout << "=> no such menu" << endl << endl << endl;
+                break;
         }
     }while(1);
 
@@ -72,7 +87,7 @@ Element scanElement(){
     cin >> newElement.name;
 
     cout << "Enter key value of element: ";
-    cin >> newElement.score;
+    cin >> newElement.score; 
 
     return newElement;
 }
@@ -82,36 +97,43 @@ void insert(Element *a, int &i, Element newElement){
 
     decrease(a, i, newElement.score);
 
-    cout << "New element [" << newElement.name << ", " << newElement.score << "] is inserted." << endl << endl << endl;
+    cout << "=> New element [" << newElement.name << ", " << newElement.score << "] is inserted." << endl << endl << endl;
 }
 
-void decreaseScore(Element *a){
+void decreaseScore(Element *a, int count){
     int index;
     float score;
 
     cout << "Enter index of element: ";
     cin >> index;
 
-    cout << "Enter new key value: ";
-    cin >> score;
+    if(index > count){
+        cout << "=> no data in index #" << index << endl << endl << endl;
+    }else{
+        cout << "Enter new key value: ";
+        cin >> score;
 
-    decrease(a, index, score);
-
-    cout << endl << endl << endl;
+        if(decrease(a, index, score)==1){
+            cout << "=> index #" << index << " element decreased to " << score << endl << endl << endl;
+        }
+    }
 }
 
-void decrease(Element *a, int i, float newScore){
+int decrease(Element *a, int i, float newScore){
     int index = i;
     
     if(newScore > a[index].score){
-        cout << "Error : new key is bigger than current key" << endl;
+        cout << "=> Error : new key is bigger than current key" << endl << endl << endl;;
+        return 0;
     }else{
         a[index].score = newScore;
         while(index > 1 && a[index/2].score > a[index].score){
             swap(a[index/2], a[index]);
             index = index/2;
         }
+        return 1;
     }
+    return 1;
 }
 
 void swap(Element *a, Element *b){
@@ -123,14 +145,14 @@ void swap(Element *a, Element *b){
 void extractMin(Element *a, int &i){
     Element min;
     if(i < 1){
-        cout << "Error : heap overflow";
+        cout << "=> Error : heap overflow" << endl << endl << endl;
     }else{
         min = a[1];
         a[1] = a[i];
         i--;
         minHeapify(a, i, 1);
     }
-    cout << "[" << min.name << ", " << min.score << "] is deleted." << endl << endl << endl;
+    cout << "=> [" << min.name << ", " << min.score << "] is deleted." << endl << endl << endl;
 }
 
 void minHeapify(Element *a, int size, int i){
